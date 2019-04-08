75a102d01c5e5d4add3e1fd77a835467a09e789f
Satsuzaki <41466960+Satsuzaki@users.noreply.github.com>
2019-04-08 09:35:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -445,7 +445,7 @@ bot.on("message", function(message) {
     case "desc":
         if (message.author.id === "213322033692409857") {
             message.delete().catch();
-            message.guild.channels.find("id", "462949032226979850").sendMessage("Salutation chers élèves, je me nomme Nesturako Seijuro. Je suis professeur de programmation au lycée **Alafia**. Je suis encore à mes débuts dans ma carrière d'enseignant donc si vous avez des suggetions à faire. Faites les par mp à ..., s'il vous plait ^^'");
+            message.guild.channels.find("id", "462949032226979850").sendMessage("Salutation chers élèves, je me nomme Nesturako Seijuro. Je suis professeur de programmation au lycée **Alafia**. Je suis encore à mes débuts dans ma carrière d'enseignant donc si vous avez des suggetions à faire. Faites les par mp à <@213322033692409857> , s'il vous plait ^^'");
         }
     break;
     case "story":
