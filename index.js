d5db8524c4faa483da64c0148b359a34d45249fd
Satsuzaki <41466960+Satsuzaki@users.noreply.github.com>
2019-05-13 14:15:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -561,12 +561,11 @@ bot.on("message", function(message)
         .setColor("#D9C400")
         message.channel.sendEmbed(embed)
     break;
-    /*
+    */
     case "helpop":
         message.delete().catch();
-        message.guild.channels.find("id", "462928491831230484").sendMessage(/*<@369914503892041730> , <@316672290479931392> ,"<@&462952083746717697> et <@&462952216064425984>: " + message.member.toString() + " à besoin d'aide !");
+        message.guild.channels.find("id", "462928491831230484").sendMessage(/*<@369914503892041730> , <@316672290479931392> ,*/"<@&462952083746717697> et <@&462952216064425984>: " + message.member.toString() + " à besoin d'aide !");
     break;
-    */
     case "ping":
         message.channel.sendMessage("Temps de latence avec le serveur: `" + `${(Date.now () - message.createdTimestamp) / 100}` + "ms`");
     break;
